20d46090a2cee46cec594e4cdf28d89efc88e51b
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-11-01 14:04:52 -0300

    Update index.jsx

--- src/components/ListaSuspensa/index.jsx
+++ src/components/ListaSuspensa/index.jsx
@@ -1,33 +1,30 @@
 import styled from "styled-components"
 
 const ListaEstilizada = styled.div`
-    margin-left: -60px;
-    margin-right: -200px;
-    min-height: 8vh;
-    justify-content: space-evenly;
+    margin: 10px 0;
+
 `
 const Label = styled.label`
     color: black;
-    display: flex;
-    margin-bottom: 8px;
+    display: block;
+    margin-bottom: 2px;
     font-size: 24px;
 `
 const Select = styled.select`
     background: #FFFFFF;
     border-radius: 10px;
-    font-size: 24px;
+    border: 1px solid rgba(0, 0, 0, 0.01);
+    font-size: 20px;
     padding: 15px;
-    box-sizing: border-box;
+    width: 90%;
     
 `
 const Option = styled.option`
     background: #FFFFFF;
     border-radius: 10px;
-    width: 90%;
     border: 1px solid rgba(0, 0, 0, 0.01);
     font-size: 20px;
-    padding: 20px;
-    box-sizing: border-box;
+    padding: 15px;
 `
 
 function ListaSuspensa({ label, options }) {
