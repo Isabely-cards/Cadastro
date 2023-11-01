c26a010e0f59c85ac0c996b1d05ac99aa7df9eeb
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-11-01 14:04:53 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRoutes from './routes.jsx'
-import styles from './styles.css'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
